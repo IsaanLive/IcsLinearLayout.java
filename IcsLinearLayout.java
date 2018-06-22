306c2506b4fd8122edb3a718daeb01b597cf92b0
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 10:59:37 +0700

    Update IcsLinearLayout.java

--- IcsLinearLayout.java
+++ IcsLinearLayout.java
@@ -1,4 +1,12 @@
-package com.sentosatech.worldcup2014de.indicator;
+/**
+ * A simple extension of a regular linear layout that supports the divider API
+ * of Android 4.0+. The dividers are added adjacent to the children by changing
+ * their layout params. If you need to rely on the margins which fall in the
+ * same orientation as the layout you should wrap the child in a simple
+ * {@link android.widget.FrameLayout} so it can receive the margin.
+ * by Wolfgang Holzem
+ */
+package com.your.packagename.indicator;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -7,13 +15,6 @@ import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.LinearLayout;
 
-/**
- * A simple extension of a regular linear layout that supports the divider API
- * of Android 4.0+. The dividers are added adjacent to the children by changing
- * their layout params. If you need to rely on the margins which fall in the
- * same orientation as the layout you should wrap the child in a simple
- * {@link android.widget.FrameLayout} so it can receive the margin.
- */
 class IcsLinearLayout extends LinearLayout {
     private static final int[] LL = new int[] {
         /* 0 */ android.R.attr.divider,
